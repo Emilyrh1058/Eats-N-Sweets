b6cebd3b8bbb3c0d6d89c57f77798e56600fb551
Emily Salinas <emilyrh1058@gmail.com>
2021-04-04 12:40:24 -0500

    include sticky navbar

--- assets/script/script.js
+++ assets/script/script.js
@@ -18,6 +18,20 @@ const createImagePlacement = document.getElementById("image");
 const createIngridientsPlacement = document.getElementById("ingridients");
 const buttonSection = document.getElementById("likebutton");
 
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
 var onClickCuisineMexico = function (event) {
   event.preventDefault();
   var mexicanButtonValue = buttonValue.value;
